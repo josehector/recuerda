950246ee240d0ae7dc544cf33cff2fb3abda7e83
JoseHector1 <JoseHector1@gmail.com>
2013-09-26 19:09:43 +0000

    continuando el desarrollo de la grabacion de audio.

--- recuerda/src/es/app/recuerda/AsistenteTwoActivity.java
+++ recuerda/src/es/app/recuerda/AsistenteTwoActivity.java
@@ -1,10 +1,17 @@
 package es.app.recuerda;
 
+import java.io.File;
+import java.io.IOException;
+
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.text.AndroidCharacter;
@@ -16,6 +23,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -24,13 +32,23 @@ public class AsistenteTwoActivity extends Activity implements OnCompletionListen
 	private static final String TAG = "AsistenteTwo";
 	
 	private AlertDialog.Builder builder;
+	private MediaRecorder recorder;
+    private MediaPlayer player;
+    private File archivo;
+    private Button btnGrabarParar;
+    private ProgressBar progressBar;
+    private Handler mHandler = new Handler();
+    private int mProgressStatus = 0;
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_asistente_two);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+		progressBar = (ProgressBar) findViewById(R.id.pbAudio);
 		
+		//btnGrabarParar = findViewById(R.id.imgBtnGrabParar);
 		builder = new AlertDialog.Builder(this);
 		builder.setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
 			
@@ -93,10 +111,94 @@ public class AsistenteTwoActivity extends Activity implements OnCompletionListen
 	            return super.onOptionsItemSelected(item);
 	    }
 	}
+	
+	public void grabarParar(View v) {
+		Log.i(TAG, "Iniciamos la grabacion");
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        File path = new File(Environment.getExternalStorageDirectory()
+                .getPath());
+        try {
+            archivo = File.createTempFile("temporal", ".3gp", path);
+            Log.i(TAG, archivo.getPath());
+        } catch (IOException e) {
+        }
+        recorder.setOutputFile(archivo.getAbsolutePath());
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+        }
+        recorder.start();
+        ImageButton btnGrabarParar = (ImageButton) v;
+        btnGrabarParar.setImageDrawable(getResources().getDrawable(R.drawable.player_stop));
+        btnGrabarParar.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Log.i(TAG, "Paramos la grabación");
+				detener(v);		     
+				
+			}
+		});
+    }
+	
+	public void detener(View v) {
+        recorder.stop();
+        recorder.release();
+        player = new MediaPlayer();
+        player.setOnCompletionListener(this);
+        try {
+            player.setDataSource(archivo.getAbsolutePath());
+        } catch (IOException e) {
+        }
+        try {
+            player.prepare();
+        } catch (IOException e) {
+        }  
+        ImageButton btnGrabarParar = (ImageButton) v;
+        btnGrabarParar.setImageDrawable(getResources().getDrawable(R.drawable.microphone2));
+    }
+
+	
+	public void reproducir(View v) {
+        Log.i(TAG, "Reproducir -> " + player.getDuration());
+		player.start();        
+        
+		
+		final int duracion = player.getDuration() / 1000;
+        progressBar.setProgress(0);
+        progressBar.setMax(duracion);
+        
+        
+        new Thread(new Runnable() {
+            public void run() {
+                while (mProgressStatus < duracion) {
+                    mProgressStatus = player.getCurrentPosition() / 1000;
+
+                    try {
+                    		Thread.sleep(500);
+                    	} catch (InterruptedException e) {
+                    		e.printStackTrace();
+                    	}
+                    
+                    // Update the progress bar
+                    mHandler.post(new Runnable() {
+                        public void run() {
+                        	progressBar.setProgress(mProgressStatus);
+                        }
+                    });
+                }
+            }
+        }).start();
+
+    }
 
 	@Override
 	public void onCompletion(MediaPlayer arg0) {
 		// TODO Auto-generated method stub
+		Log.i(TAG, "Completado");
 		
 	}
 
