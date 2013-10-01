4e2a70ff66dbaa9a9e6c166388b9ba9913952c60
JoseHector1@gmail.com <JoseHector1@gmail.com@71b98a3c-526b-c980-8ddb-3688957dd4ee>
2013-10-01 18:15:08 +0000

    Continuado el desarrollo de la segunda parte del asistente.

---
+++ recuerda/src/es/app/recuerda/exception/BBDDException.java
@@ -0,0 +1,25 @@
+package es.app.recuerda.exception;
+
+public class BBDDException extends Exception {
+
+	public BBDDException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BBDDException(String detailMessage, Throwable throwable) {
+		super(detailMessage, throwable);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BBDDException(String detailMessage) {
+		super(detailMessage);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BBDDException(Throwable throwable) {
+		super(throwable);
+		// TODO Auto-generated constructor stub
+	}
+
+}
