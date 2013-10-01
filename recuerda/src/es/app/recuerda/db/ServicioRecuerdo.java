85c2b560daf68239c00f8b528e4d01f1a3bf63a9
JoseHector1@gmail.com <JoseHector1@gmail.com@71b98a3c-526b-c980-8ddb-3688957dd4ee>
2013-10-01 18:43:57 +0000

    Añadido al servicio obtener la lista de todos los recuerdos, imagenes y audio incluidos.

--- recuerda/src/es/app/recuerda/db/ServicioRecuerdo.java
+++ recuerda/src/es/app/recuerda/db/ServicioRecuerdo.java
@@ -8,11 +8,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
 import android.os.Environment;
 import android.util.Log;
 
@@ -27,26 +29,62 @@ import es.app.recuerda.util.Constantes;
 
 public class ServicioRecuerdo {
 	private static final String TAG = "ServicioRecuerdo";
-	
+
 	private DBRecuerdo dbRecuerdo;
-	
+
 	public ServicioRecuerdo(Context context) {
 		dbRecuerdo = OpenHelperManager.getHelper(context, DBRecuerdo.class);
-	}	
-	
+	}
+
+	public List<WraperRecuerdo> getListaRecuerdos() {
+		List<WraperRecuerdo> listaRecuerdos = null;
+		try {
+			Dao<Recuerdo, Integer> daoRecuerdo = dbRecuerdo
+					.getDao(Recuerdo.class);
+			List<Recuerdo> lista = daoRecuerdo.queryForAll();
+			listaRecuerdos = new ArrayList<WraperRecuerdo>();
+			for (Recuerdo recuerdo : lista) {
+				String pathImg = Constantes.RUTA_APP + "/"
+						+ Constantes.PREFIJO_IMG + recuerdo.getId()
+						+ Constantes.EXTENSION_IMG;
+				Bitmap imagen = null;
+				try {
+					FileInputStream fileImg = new FileInputStream(pathImg);
+					imagen = BitmapFactory.decodeStream(fileImg, null, null);
+				} catch (FileNotFoundException e) {
+					Log.e(TAG, "No se ha encontrado la imagen asociada al recuerdo");
+				}
+				String pathAudio = Constantes.RUTA_APP + "/"
+				+ Constantes.PREFIJO_AUDIO + recuerdo.getId()
+				+ Constantes.EXTENSION_AUDIO;
+				
+				File fileAudio = new File(pathAudio);
+				if(!fileAudio.exists()) {
+					Log.i(TAG, "No existe audio asociado al recuerdo");
+					fileAudio = null;
+				}				
+				WraperRecuerdo wRecuerdo = new WraperRecuerdo(recuerdo, imagen, fileAudio);
+				listaRecuerdos.add(wRecuerdo);
+			}
+		} catch (SQLException e) {
+			Log.e(TAG, "Error al recuperar los recuerdos");
+		}
+		return listaRecuerdos;
+	}
+
 	public List<Relacion> getListaRelacion() {
 		List<Relacion> lista = null;
 		try {
 			Dao<Relacion, Integer> daoRelacion = dbRecuerdo
-			.getDao(Relacion.class);
+					.getDao(Relacion.class);
 			lista = daoRelacion.queryForAll();
 		} catch (SQLException e) {
-			Log.e(TAG, e.getMessage());			
+			Log.e(TAG, e.getMessage());
 		}
 		return lista;
 	}
-	
-	public void guardar(Relacion relacion) throws BBDDException{
+
+	public void guardar(Relacion relacion) throws BBDDException {
 		try {
 			if (relacion.getId() == -1) {
 				Dao<Relacion, Integer> daoRelacion = dbRecuerdo
@@ -58,22 +96,25 @@ public class ServicioRecuerdo {
 			throw new BBDDException("Error al guardar la relacion");
 		}
 	}
-	
-	public void guardar(WraperRecuerdo wpRecuerdo) throws BBDDException{
+
+	public void guardar(WraperRecuerdo wpRecuerdo) throws BBDDException {
 		Recuerdo recuerdo = wpRecuerdo.getRecuerdo();
 		Relacion relacion = recuerdo.getRelacion();
 		try {
 			if (relacion.getId() == -1) {
-				Dao<Relacion, Integer> daoRelacion = dbRecuerdo.getDao(Relacion.class);
+				Dao<Relacion, Integer> daoRelacion = dbRecuerdo
+						.getDao(Relacion.class);
 				daoRelacion.create(relacion);
 			}
-			Dao<Recuerdo, Integer> daoRecuerdo = dbRecuerdo.getDao(Recuerdo.class);
+			Dao<Recuerdo, Integer> daoRecuerdo = dbRecuerdo
+					.getDao(Recuerdo.class);
 			int idRecuerdo = daoRecuerdo.create(recuerdo);
 			Log.i(TAG, "idRecuerdo: " + recuerdo.getId());
-			storeImage(wpRecuerdo.getImagen(), Constantes.PREFIJO_IMG + recuerdo.getId() + Constantes.EXTENSION_IMG);	
+			storeImage(wpRecuerdo.getImagen(), Constantes.PREFIJO_IMG
+					+ recuerdo.getId() + Constantes.EXTENSION_IMG);
 			if (wpRecuerdo.getAudio() != null) {
-				storeFile(wpRecuerdo.getAudio(), Constantes.PREFIJO_AUDIO + recuerdo.getId()
-						+ Constantes.EXTENSION_AUDIO);
+				storeFile(wpRecuerdo.getAudio(), Constantes.PREFIJO_AUDIO
+						+ recuerdo.getId() + Constantes.EXTENSION_AUDIO);
 			}
 			Log.i(TAG, "Recuerdo guardado correctamente.");
 		} catch (SQLException e) {
@@ -82,51 +123,37 @@ public class ServicioRecuerdo {
 			throw new BBDDException("Error al guardar WraperRecuerdo");
 		}
 	}
-	
-	/*public Relacion getRelacion(int idRelacion) throws BBDDException{
-		Relacion relacion = null;
-		try {
-			Dao<Relacion, Integer> daoRelacion = dbRecuerdo.getDao(Relacion.class);
-			relacion = daoRelacion.queryForId(idRelacion);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			Log.e(TAG, e.getMessage());
-			throw new BBDDException("Error al obtener la relacion: " + idRelacion);
-		}
-		return relacion;
-	}*/
-	
+
 	public void cerrar() {
 		if (dbRecuerdo != null) {
 			OpenHelperManager.releaseHelper();
 			dbRecuerdo = null;
 		}
 	}
-	
-	private boolean storeFile(File audio, String filename) {		
+
+	private boolean storeFile(File audio, String filename) {
 		String filePath = Constantes.RUTA_APP + "/" + filename;
 		BufferedOutputStream bufDestino = null;
 		BufferedInputStream bufOrigen = null;
 		try {
-			
+
 			FileOutputStream fileDestino = new FileOutputStream(filePath);
 			bufDestino = new BufferedOutputStream(fileDestino);
-			
+
 			FileInputStream fileOrigen = new FileInputStream(audio);
-			bufOrigen = new BufferedInputStream(fileOrigen);					
-			
-			 // Bucle para leer de un fichero y escribir en el otro.
-            byte [] array = new byte[1000];
-            int leidos = bufOrigen.read(array);
-            while (leidos > 0)
-            {
-            	bufDestino.write(array,0,leidos);
-                leidos=bufOrigen.read(array);
-            }
- 
-            // Cierre de los ficheros
-            //bufDestino.close();
-            //bufOrigen.close();
+			bufOrigen = new BufferedInputStream(fileOrigen);
+
+			// Bucle para leer de un fichero y escribir en el otro.
+			byte[] array = new byte[1000];
+			int leidos = bufOrigen.read(array);
+			while (leidos > 0) {
+				bufDestino.write(array, 0, leidos);
+				leidos = bufOrigen.read(array);
+			}
+
+			// Cierre de los ficheros
+			// bufDestino.close();
+			// bufOrigen.close();
 		} catch (FileNotFoundException e) {
 			Log.e(TAG, e.getMessage());
 		} catch (IOException e) {
@@ -145,24 +172,25 @@ public class ServicioRecuerdo {
 		}
 		return true;
 	}
-	
-	private void prepare() {		
+
+	private void prepare() {
 		File sdIconStorageDir = new File(Constantes.RUTA_APP);
-		//create storage directories, if they don't exist
+		// create storage directories, if they don't exist
 		sdIconStorageDir.mkdirs();
 
 	}
-	
+
 	private boolean storeImage(Bitmap imageData, String filename) {
-		//get path to external storage (SD card)
-		prepare();		
+		// get path to external storage (SD card)
+		prepare();
 		try {
 			String filePath = Constantes.RUTA_APP + "/" + filename;
 			FileOutputStream fileOutputStream = new FileOutputStream(filePath);
 
-			BufferedOutputStream bos = new BufferedOutputStream(fileOutputStream);
+			BufferedOutputStream bos = new BufferedOutputStream(
+					fileOutputStream);
 
-			//choose another format if PNG doesn't suit you
+			// choose another format if PNG doesn't suit you
 			imageData.compress(CompressFormat.PNG, 100, bos);
 
 			bos.flush();
