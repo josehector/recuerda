fcc826d8df235a74b141e2c721809642e1253ccc
JoseHector1 <JoseHector1@gmail.com>
2013-10-25 16:39:38 +0000

    Controlar una instalación anterior

--- recuerda/src/es/app/recuerda/db/DBRecuerdo.java
+++ recuerda/src/es/app/recuerda/db/DBRecuerdo.java
@@ -1,9 +1,11 @@
 package es.app.recuerda.db;
 
+import java.io.File;
 import java.sql.SQLException;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.dao.Dao;
@@ -13,9 +15,11 @@ import com.j256.ormlite.table.TableUtils;
 import es.app.recuerda.R;
 import es.app.recuerda.entidades.Recuerdo;
 import es.app.recuerda.entidades.Relacion;
+import es.app.recuerda.util.Constantes;
 
 public class DBRecuerdo extends OrmLiteSqliteOpenHelper{
 
+	private static final String TAG = "DBRecuerdo";
 	private static final String DATABASE_NAME = "recuerdo.db";
     private static final int DATABASE_VERSION = 1;
  
@@ -31,6 +35,7 @@ public class DBRecuerdo extends OrmLiteSqliteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource) {
         try {
+        	prepare();
             TableUtils.createTable(connectionSource, Recuerdo.class);
             TableUtils.createTable(connectionSource, Relacion.class);
             for(int i = 0; i<relaciones.length; i++) {
@@ -41,6 +46,24 @@ public class DBRecuerdo extends OrmLiteSqliteOpenHelper{
             throw new RuntimeException(e);
         }
     }
+    
+    /**
+     * Prepara la carpeta que guardará las imagenes y los audios.
+     * Elimina los ficheros si hubiera una instalación antigua.
+     */
+    private void prepare() {
+		File carpetaRecuerda = new File(Constantes.RUTA_APP);
+		if (carpetaRecuerda.exists()) {
+			Log.i(TAG, "Ya existe la carpeta creada.");
+			File[] archivos = carpetaRecuerda.listFiles();
+			for (File archivo : archivos) {
+				boolean valor = archivo.delete();
+				Log.i(TAG, "Borrando fichero " + archivo.getPath() + "......." + (valor ? "OK" : "No se pudo."));
+			}
+		} else {
+			carpetaRecuerda.mkdirs();
+		}
+	}
  
     @Override
     public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVersion, int newVersion) {
