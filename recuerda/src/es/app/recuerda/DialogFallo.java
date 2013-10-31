527938399368fd2554bc1e2016ac1bf305ec4751
JoseHector1 <JoseHector1@gmail.com>
2013-10-31 12:26:05 +0000

    Desarrollo del juego de memoria

--- recuerda/src/es/app/recuerda/DialogFallo.java
+++ recuerda/src/es/app/recuerda/DialogFallo.java
@@ -5,31 +5,33 @@ import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class DialogFallo extends DialogFragment {
 	private static final String TAG = DialogCorrecto.class.getName();
 
-	public static DialogFallo newInstance(int msg) {
+	public static DialogFallo newInstance(int[] msg) {
 		DialogFallo frag = new DialogFallo();
 		Bundle args = new Bundle();
-		args.putInt("msg", msg);
+		args.putIntArray("msg", msg);
 		frag.setArguments(args);
 		return frag;
 	}
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		int msg = getArguments().getInt("msg");
+		int[] msg = getArguments().getIntArray("msg");
 		Dialog dialogFallo = new Dialog(getActivity(), android.R.style.Theme_Translucent);
 		dialogFallo.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		dialogFallo.setContentView(R.layout.dialog_fallo);
 		TextView txtView = (TextView) dialogFallo.findViewById(R.id.tvDialogFallo);
-		txtView.setText(msg);
+		txtView.setText(msg[0]);
+		TextView txtView2 = (TextView) dialogFallo.findViewById(R.id.tvDialogFallo2);
+		txtView2.setText(msg[1]);
 		
-		ImageButton imgBtn = (ImageButton) dialogFallo.findViewById(R.id.btnOkDialogFallo);
-	    imgBtn.setOnClickListener(new View.OnClickListener() {			
+		LinearLayout llDialogFallo = (LinearLayout) dialogFallo.findViewById(R.id.llDialogFallo);		
+		llDialogFallo.setOnClickListener(new View.OnClickListener() {			
 			@Override
 			public void onClick(View v) {
 				dismiss();				
--- recuerda/src/es/app/recuerda/RecuerdaApp.java
+++ recuerda/src/es/app/recuerda/RecuerdaApp.java
@@ -1,19 +1,32 @@
 package es.app.recuerda;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import es.app.recuerda.entidades.Recuerdo;
 import es.app.recuerda.entidades.WraperRecuerdo;
+import es.app.recuerda.util.Util;
 
 import android.app.Application;
 
 public class RecuerdaApp extends Application {
+	private static final int NUM_FRASES = 2;
 	private List<Recuerdo> recuerdos;
 	private Partida partida;
+	private Map<Integer, Integer> mapaFrases;
 	
 	public RecuerdaApp() {
 		recuerdos = null;
 		partida = new Partida();
+		inicializarFrases();
+		
+	}
+	
+	private void inicializarFrases() {
+		mapaFrases = new HashMap<Integer, Integer>(NUM_FRASES);
+		mapaFrases.put(R.string.frase1, R.string.autor_frase1);
+		mapaFrases.put(R.string.frase2, R.string.autor_frase2);
 	}
 
 	public List<Recuerdo> getRecuerdos() {
@@ -41,6 +54,33 @@ public class RecuerdaApp extends Application {
 
 	public void setPartida(Partida partida) {
 		this.partida = partida;
+	}
+	
+	/**
+	 * Devuelve una frase y autor aleatorios (sobre la memoria).
+	 * 
+	 * @return Array con los identifcadores de la frase y su autor.
+	 */
+	public int[] getFrase() {
+		int aleatorio = Util.aleatorio(0, NUM_FRASES - 1);
+		int frase = (Integer) mapaFrases.keySet().toArray()[aleatorio];
+		int autor = mapaFrases.get(frase);
+		int[] valor = new int[2];
+		valor[0] = frase;
+		valor[1] = autor;
+		return valor;		
+	}
+	
+	/**
+	 * Devuelve una frase y autor aleatorios (sobre errores).
+	 * 
+	 * @return Array con los identifcadores de la frase y su autor.
+	 */
+	public int[] getFraseError() {
+		int[] valor = new int[2];
+		valor[0] = R.string.frase1_error;
+		valor[1] = R.string.autor_frase1_error;
+		return valor;
 	}
 	
 }
