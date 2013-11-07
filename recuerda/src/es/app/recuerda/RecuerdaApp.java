c4f6dd3a6757ee28813ab9562a7dd1dea226fd82
JoseHector1@gmail.com <JoseHector1@gmail.com@71b98a3c-526b-c980-8ddb-3688957dd4ee>
2013-11-07 17:16:18 +0000

    Nuevas frases

--- recuerda/src/es/app/recuerda/RecuerdaApp.java
+++ recuerda/src/es/app/recuerda/RecuerdaApp.java
@@ -9,7 +9,7 @@ import es.app.recuerda.entidades.Recuerdo;
 import es.app.recuerda.util.Util;
 
 public class RecuerdaApp extends Application {
-	private static final int NUM_FRASES = 6;
+	private static final int NUM_FRASES = 27;
 	private List<Recuerdo> recuerdos;
 	private Partida partida;
 	private Map<Integer, Integer> mapaFrases;
@@ -29,6 +29,32 @@ public class RecuerdaApp extends Application {
 		mapaFrases.put(R.string.frase4, R.string.autor_frase4);
 		mapaFrases.put(R.string.frase5, R.string.autor_frase5);
 		mapaFrases.put(R.string.frase6, R.string.autor_frase6);
+		
+		mapaFrases.put(R.string.frase7, R.string.autor_frase7);
+		mapaFrases.put(R.string.frase8, R.string.autor_frase8);
+		mapaFrases.put(R.string.frase9, R.string.autor_frase9);
+		mapaFrases.put(R.string.frase10, R.string.autor_frase10);
+		mapaFrases.put(R.string.frase11, R.string.autor_frase11);
+		mapaFrases.put(R.string.frase12, R.string.autor_frase12);
+		
+		mapaFrases.put(R.string.frase13, R.string.autor_frase13);
+		mapaFrases.put(R.string.frase14, R.string.autor_frase14);
+		mapaFrases.put(R.string.frase15, R.string.autor_frase15);
+		mapaFrases.put(R.string.frase16, R.string.autor_frase16);
+		mapaFrases.put(R.string.frase17, R.string.autor_frase17);
+		mapaFrases.put(R.string.frase18, R.string.autor_frase18);
+		
+		mapaFrases.put(R.string.frase19, R.string.autor_frase19);
+		mapaFrases.put(R.string.frase20, R.string.autor_frase20);
+		mapaFrases.put(R.string.frase21, R.string.autor_frase21);
+		mapaFrases.put(R.string.frase22, R.string.autor_frase22);
+		mapaFrases.put(R.string.frase23, R.string.autor_frase23);
+		mapaFrases.put(R.string.frase24, R.string.autor_frase24);
+		
+		mapaFrases.put(R.string.frase25, R.string.autor_frase25);
+		mapaFrases.put(R.string.frase26, R.string.autor_frase26);
+		mapaFrases.put(R.string.frase27, R.string.autor_frase27);
+		
 	}
 
 	public List<Recuerdo> getRecuerdos() {
